15a179ac5283052494bd26283cdf3eaad721971d
FWiman <frewim@gmail.com>
2025-06-03 11:16:09 +0200

    application.properties, application-production.properties, layouttemplate.html, pom.xml, WebSecurityConfig.java changes

--- src/main/java/se/systementor/supershoppen1/shop/configuration/WebSecurityConfig.java
+++ src/main/java/se/systementor/supershoppen1/shop/configuration/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -43,8 +44,7 @@ public class WebSecurityConfig {
             .oauth2Login(oauth2 -> oauth2
                 .loginPage("/login")
             )
-            .logout(logout -> logout
-                .permitAll()
+            .logout(LogoutConfigurer::permitAll
             );
         return http.build();
     }
